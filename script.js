e107172687dc0b84219593ff4f0eff9dc698b15b
Yacho-glitch <181276632+Yacho-glitch@users.noreply.github.com>
2025-08-01 21:49:20 +0100

    Add the script to empty javascript file

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-"use strict";
+// Filter JS
 
-
-let handling = () => {
-    let element = document.querySelector('h1');
-
-    element.innerHTML = "Hello, Tailwind CSS!";
-}
-
+$(document).ready(function() {
+    $('.filter-item').click(function() {
+        const value = $(this).attr('data-filter');
+        if (value == "all") {
+            $('.post-box').show('1000');
+        }
+        else {
+            $('.post-box').not('.' + value).hide('1000');
+            $('.post-box').filter('.' + value).show('1000');
+        }
+    })
+})
 
